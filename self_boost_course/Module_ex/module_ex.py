2bbc2454edad1ba26087b7ef88cf8f4f7b6731cc
calixlock <calixlock@naver.com>
2021-11-10 16:40:16 +0900

    .

--- self_boost_course/Module_ex/module_ex.py
+++ self_boost_course/Module_ex/module_ex.py
@@ -1,6 +1,8 @@
-import fah_converter
-
-print("Enter a celcius value:")
-c = float(input())
-f = fah_converter.convert_c_to_f(c)
-print(f"That's {f} degrees fahrenheit ") 
+# import fah_converter as fah
+from fah_converter import convert_c_to_f as cf 
+if __name__ == "__main__" :
+    print("Enter a celcius value:")
+    c = float(input())
+    # f = fah.convert_c_to_f(c)
+    f = cf(c)
+    print(f"That's {f} degrees fahrenheit ") 
